f19f6418e04588fc1f4bc4f60379d2e351496c1a
Andrew Schmid <andrew.schmid123@gmail.com>
2023-02-16 17:33:24 -0600

    Without LSP

---
+++ nvim/.config/nvim/lua/andrew/init.lua
@@ -0,0 +1,7 @@
+require("andrew.packer")
+require("andrew.remap")
+require("andrew.set")
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
---
+++ nvim/.config/nvim/lua/andrew/remap.lua
@@ -0,0 +1,3 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", function() vim.cmd("Ex") end)
+
