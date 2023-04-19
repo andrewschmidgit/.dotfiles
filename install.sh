5e12d3cc51ab69742aef52fa8359fa0ed420c324
Andrew Schmid <andrew.schmid123@gmail.com>
2023-04-19 16:07:29 -0500

    modularization

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ apt-get upgrade
 
 while read -r line
 do
-	[ -z "$line" ] && continue
-	apt-get install -y -qq "$line"
+    [ -z "$line" ] && continue
+    apt-get install -y -qq "$line"
 done < "$FILE"
 
 # change shell to zsh
@@ -27,50 +27,35 @@ echo "run > 'chsh -s $(which zsh)'"
 
 if [ true ]
 then
-#!/bin/sh
+    #!/bin/sh
 
-repeat() {
-	for i in {1..$2}; do echo "$1"; done
-}
+    repeat() {
+        for i in {1..$2}; do echo "$1"; done
+    }
 
-line() { echo $(repeat '-' 80); }
+    line() { echo $(repeat '-' 80); }
 
 # node
 node -v
 
 if [ $? -ne 0 ]
 then
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh" | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-nvm install node
+    curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh" | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    nvm install node
 fi
 
 # rust
 which rustc
 if [ $? -ne 0 ]
 then
-line
-echo "installing rust"
-line
-curl https://sh.rustup.rs -sSf | sh
-source "$HOME/.cargo/env"
+    line
+    echo "installing rust"
+    line
+    curl https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
 fi
 fi
 
-#################
-### Configure ###
-#################
-#!/usr/bin/env zsh
-DOTFILES=$HOME/.dotfiles
-STOW_FOLDERS="zsh,nvim"
-
-cd $DOTFILES
-for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
-do
-	echo "stow $folder"
-	stow -D $folder
-	stow $folder
-done
-
-
+./install.sh
