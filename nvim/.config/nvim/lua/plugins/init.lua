16b3a2f18a1de719fc8c5d3df48243d1e81b6ee4
Andrew Schmid <andrewschmid123@gmail.com>
2023-04-27 17:15:14 -0500

    working on nvim

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,37 +1,15 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-local plugins = {
-    {
-        -- File finding
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.1',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+return {
     {
         "catppuccin/nvim",
         name = "catppuccin"
     }, -- Theme
     'nvim-lualine/lualine.nvim',
-    'theprimeagen/harpoon',
     'theprimeagen/vim-be-good',
-    'tpope/vim-fugitive',
     {
-        'windwp/nvim-autopairs',
-        config = function() require('nvim-autopairs').setup {} end
+        'tpope/vim-fugitive',
+        keys = {
+            { '<leader>gs', '<cmd>Git<enter>' }
+        }
     },
     {
         'nvim-treesitter/nvim-treesitter',
@@ -57,6 +35,9 @@ local plugins = {
     {
         "nvim-neo-tree/neo-tree.nvim",
         tag = '2.59',
+        keys = {
+            { '<leader>pt', '<cmd>Neotree toggle<enter>' }
+        },
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -64,5 +45,3 @@ local plugins = {
         }
     }
 }
-
-require("lazy").setup('plugins', opts)
---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    keys = {
+        { '<leader>pf', '<cmd>Telescope find_files<enter>' },
+        { '<C-p>',      '<cmd>Telescope git_files<enter>' },
+    }
+}
