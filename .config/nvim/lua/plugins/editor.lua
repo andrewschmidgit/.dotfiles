9b585b8b4ddb9f57302511123b931333a322fd67
Andrew Schmid <andrewschmid-dev@proton.me>
2024-04-05 20:32:13 -0500

    Added formatting

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,5 @@
 return {
-	{
+	{ -- pending keybinds
 		'folke/which-key.nvim',
 		event = 'VeryLazy',
 		opts = {
@@ -8,22 +8,20 @@ return {
 				['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
 				['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
 				['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-				['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
 				['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
 				['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
 				['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
 				['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 			},
 		},
-		config = function (_, opts)
+		config = function(_, opts)
 			local wk = require('which-key')
 			wk.setup(opts)
 			wk.register(opts.defaults)
 		end,
 	},
 
-	-- git signs
-	{
+	{ -- git signs
 		'lewis6991/gitsigns.nvim',
 		opts = {
 			signs = {
@@ -52,5 +50,5 @@ return {
 				map("n", "<leader>ghD", function() gs.diffthis("~") end, "Diff This ~")
 			end
 		},
-	}
+	},
 }
