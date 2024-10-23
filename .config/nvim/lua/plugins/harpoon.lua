8a86dd97fdaf1805c929898c367f673bae9b3137
Andrew Schmid <andrewschmid-dev@proton.me>
2024-10-23 16:42:54 -0500

    add: harpoon

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,14 @@
+return {
+	'ThePrimeagen/harpoon',
+	branch = 'harpoon2',
+	dependencies = { 'nvim-lua/plenary.nvim' },
+	opts = {},
+	keys = {
+		{ '<leader>a', function() require('harpoon'):list():add() end, mode = 'n', desc = 'Add to jump list' },
+		{ '<C-e>', function() local h = require('harpoon'); h.ui:toggle_quick_menu(h:list()) end, mode = 'n', desc = 'Add to jump list' },
+		{ '<C-h>', function() require('harpoon'):list():select(1) end, mode = 'n', },
+		{ '<C-t>', function() require('harpoon'):list():select(2) end, mode = 'n', },
+		{ '<C-n>', function() require('harpoon'):list():select(3) end, mode = 'n', },
+		{ '<C-s>', function() require('harpoon'):list():select(4) end, mode = 'n', },
+	},
+}
