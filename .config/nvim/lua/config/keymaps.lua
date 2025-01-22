52e186e841e3e6adec87538d15508dbebe8580ad
Andrew Schmid <andrewschmid-dev@proton.me>
2025-01-22 09:35:33 -0600

    nvim: keymaps.lua organization

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,14 +9,14 @@ set('n', '<leader>de', vim.diagnostic.open_float, { desc = 'Open floating diagno
 set('n', '<leader>dq', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Visual mode, use J and K to move lines up and down
-vim.keymap.set('v', 'J', ":m '>+1<enter>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<enter>gv=gv")
+set('v', 'J', ":m '>+1<enter>gv=gv")
+set('v', 'K', ":m '<-2<enter>gv=gv")
 
 -- Leaves cursor alone when joining line below
-vim.keymap.set('n', 'J', 'mzJ`z')
+set('n', 'J', 'mzJ`z')
 
 -- Recenters cursor y-position when navigating up and down
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+set('n', '<C-d>', '<C-d>zz')
+set('n', '<C-u>', '<C-u>zz')
+set('n', 'n', 'nzzzv')
+set('n', 'N', 'Nzzzv')
