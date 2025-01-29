e9be83a3d7774638028fa3c738659e6a5590f936
Andrew Schmid <andrewschmid-dev@proton.me>
2025-01-29 16:29:09 -0600

    nvim: add keymap for which key help

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,5 +1,13 @@
 return {
 	'folke/which-key.nvim',
+	event = 'VeryLazy',
+	keys = {
+		{
+			'<leader>?',
+			function() require('which-key').show({ global = false }) end,
+			desc = 'Buffer local keymaps'
+		}
+	},
 	config = function()
 		local wk = require('which-key')
 		wk.add {
