65a9f746350159bbc0095231283b60e3c9a418b0
Andrew Schmid <andrewschmid-dev@proton.me>
2025-02-06 18:06:04 -0600

    merge(nvim): oil plugin config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,17 @@
 return {
 	'stevearc/oil.nvim',
-	dependencies = { 'echasnovski/mini.icons' },
-	config = function()
-		require('custom.oil')
-	end
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	lazy = false,
+	keys = {
+		{ '-', '<cmd>Oil<enter>', mode = { 'n' } }
+	},
+	opts = {
+		default_file_explorer = true,
+		keymaps = {
+			['<C-t>'] = false
+		},
+		view_options = {
+			show_hidden = true
+		}
+	}
 }
